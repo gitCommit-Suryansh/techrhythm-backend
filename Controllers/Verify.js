940b284daf9feaccea3f4bcfcc6b4ef3c66ba27c
BlackHat86 <suryansh.c05@gmail.com>
2025-03-06 13:34:27 +0530

    main commit

--- Controllers/Verify.js
+++ Controllers/Verify.js
@@ -14,9 +14,31 @@ exports.verifyPass = async (req, res) => {
         // Perform any operations you need with the found user
         // For example, mark the pass as verified or log the verification
 
-        return res.status(200).json({ verified: true });
+        return res.status(200).json({ verified: true ,userId:user._id});
     } catch (error) {
         console.error("Error verifying pass:", error);
         return res.status(500).json({ verified: false, error: error.message });
+    }
+};
+
+exports.checkIn = async (req, res) => {
+    const { userId } = req.body;
+
+    try {
+        // Find the user by userId
+        const user = await usermodel.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found." });
+        }
+
+        // Update the user's checkIn status
+        user.checkedIn = true;
+        await user.save();
+
+        return res.status(200).json({ message: "Check-in successful."});
+    } catch (error) {
+        console.error("Error during check-in:", error);
+        return res.status(500).json({ message: "Error during check-in.", error: error.message });
     }
 };
