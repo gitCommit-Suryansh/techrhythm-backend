bf7b5380d4561b299a1432a45a746bde2fb3fc8a
BlackHat86 <suryansh.c05@gmail.com>
2025-03-20 21:10:46 +0530

    main commit

--- model/user.js
+++ model/user.js
@@ -6,7 +6,7 @@ const userSchema=mongoose.Schema({
     college:{type:String,required:true},
     phone:{type:String,required:true},
     password:{type:String,required:true},
-    passType:{type:String,enum:['SILVER','GOLD','BRONZE'],required:false},
+    passType:{type:String,enum:['SILVER','GOLD','BRONZE','PLATINUM'],required:false},
     passAmount:{type:String,required:false},
     passId:{type:String,default:null,required:false},
     checkedIn:{type:Boolean,default:false}
